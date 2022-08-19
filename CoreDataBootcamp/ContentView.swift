77e307bf3d56ee9e53021ca841d2b5a75ee3b6e0
Amir Diafi <59441376+AmirDiafi@users.noreply.github.com>
2022-08-19 10:12:25 +0100

    first commit

--- CoreDataBootcamp/ContentView.swift
+++ CoreDataBootcamp/ContentView.swift
@@ -10,71 +10,106 @@ import CoreData
 
 struct ContentView: View {
     @Environment(\.managedObjectContext) private var viewContext
-
     @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<Item>
+        entity: FruitEntity.entity(),
+        sortDescriptors: [NSSortDescriptor(keyPath: \FruitEntity.name, ascending: true)]
+    )
+    private var fruits: FetchedResults<FruitEntity>
+    @State var value: String = ""
+    @State var isUpdating: Bool = false
+    @State var updatingIndex: FruitEntity?
 
     var body: some View {
-        List {
-            ForEach(items) { item in
-                Text("Item at \(item.timestamp!, formatter: itemFormatter)")
+        NavigationView{
+            VStack{
+                VStack{
+                    TextField("Add a fruit", text: $value)
+                        .padding()
+                        .background(Color(.secondarySystemBackground))
+                        .cornerRadius(10)
+                        .padding(.horizontal)
+                    Button(action: {
+                        if isUpdating && value.count > 0 {
+                            updateItem(targetValue: updatingIndex!)
+                        } else {
+                            addItem(item: value)
+                        }
+                    }) {
+                        Text(isUpdating ? "Save" : "Add")
+                            .font(.headline)
+                            .foregroundColor(.white)
+                            .frame(maxWidth: .infinity)
+                            .padding()
+                            .background(Color.blue)
+                            .cornerRadius(10)
+                            .padding(.horizontal)
+                    }
+                }
+                
+                List {
+                    ForEach(fruits) { fruit in
+                        Text(fruit.name ?? "")
+                        .onTapGesture {
+                            withAnimation {
+                                isUpdating = true
+                                value = fruit.name ?? ""
+                                updatingIndex = fruit
+                            }
+                        }
+                    }
+                    .onDelete(perform: deleteItems)
+                }
+                .listStyle(PlainListStyle())
             }
-            .onDelete(perform: deleteItems)
-        }
-        .toolbar {
-            #if os(iOS)
-            EditButton()
-            #endif
-
-            Button(action: addItem) {
-                Label("Add Item", systemImage: "plus")
-            }
+            .navigationTitle("Core Data Bootcamp")
         }
     }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-
+    
+    private func saveItems(){
+        withAnimation{
             do {
                 try viewContext.save()
             } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
                 let nsError = error as NSError
                 fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+            }
+        }
+    }
+    
+    private func addItem(item: String) {
+        if item.count > 0 {
+            withAnimation {
+                let newFruit = FruitEntity(context: viewContext)
+                newFruit.name = item
+                saveItems()
+                value = ""
             }
         }
     }
 
     private func deleteItems(offsets: IndexSet) {
         withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
+            guard let index = offsets.first else {return}
+            let fruitEntity = fruits[index]
+            viewContext.delete(fruitEntity)
+            saveItems()
+        }
+    }
+    
+    private func updateItem(targetValue: FruitEntity){
+        withAnimation{
+            guard let index = fruits.firstIndex(of: targetValue) else {return}
+            let fruitEntity = fruits[index]
+            fruitEntity.name = value
+            value = ""
+            saveItems()
+            isUpdating = false
         }
     }
 }
 
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+        ContentView().previewDevice("iPhone 12").environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
     }
 }
